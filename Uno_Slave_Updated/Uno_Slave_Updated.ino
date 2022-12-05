dd005dc4818b91b5d6e780dead1ecc7cfb0551d8
Kiyan <dnguye07@uw.edu>
2022-12-04 22:22:22 -0800

    it finally work kekw

--- Uno_Slave_Updated/Uno_Slave_Updated.ino
+++ Uno_Slave_Updated/Uno_Slave_Updated.ino
@@ -1,34 +1,36 @@
+// #include <ArduinoWiFiServer.h>
+// #include <BearSSLHelpers.h>
+// #include <CertStoreBearSSL.h>
+// #include <ESP8266WiFi.h>
+// #include <ESP8266WiFiAP.h>
+// #include <ESP8266WiFiGeneric.h>
+// #include <ESP8266WiFiGratuitous.h>
+// #include <ESP8266WiFiMulti.h>
+// #include <ESP8266WiFiSTA.h>
+// #include <ESP8266WiFiScan.h>
+// #include <ESP8266WiFiType.h>
+// #include <WiFiClient.h>
+// #include <WiFiClientSecure.h>
+// #include <WiFiClientSecureBearSSL.h>
+// #include <WiFiServer.h>
+// #include <WiFiServerSecure.h>
+// #include <WiFiServerSecureBearSSL.h>
+// #include <WiFiUdp.h>
+
 //------------------------------- Imports -------------------------------
 /*Will need to inport Adafruit stuff*/
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303.h>
-#include <Adafruit_LSM303_U.h>
+//#include <Adafruit_LSM303.h>
+#include <Adafruit_LSM303_Accel.h>
 #include <SPI.h>
 #include <SD.h>
 #include <Stepper.h>
 
-#include <ArduinoWiFiServer.h>
-#include <BearSSLHelpers.h>
-#include <CertStoreBearSSL.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiGratuitous.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiType.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFiClientSecureBearSSL.h>
-#include <WiFiServer.h>
-#include <WiFiServerSecure.h>
-#include <WiFiServerSecureBearSSL.h>
-#include <WiFiUdp.h>
+
 //#include <WiFi.h>
-#include <espnow.h>
+// #include <espnow.h>
 #include <SoftwareSerial.h>
 #define rxPin 10 //Pin to connect to D6
 #define txPin 11 //Pin to connect to D7
@@ -81,6 +83,9 @@ bool start = false;
 int manual = 0;
 bool ack = false;
 
+
+bool newData = false;
+
 //------------------------------- Pins Define -------------------------------
 //Ultrasonic pins
 #define echoPin 5
@@ -98,13 +103,22 @@ bool ack = false;
 
 //------------------------------- Setup -------------------------------
 //Setup Accel Unquique ID
-Adafruit_LSM303_Mag_Unified accel = Adafruit_LSM303_Mag_Unified(54321);
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 
 //Set up the Stepper motor variable
 Stepper stepper(stepsPerRevolution, in1, in2, in3, in4);
 void setup() {
   //setup serial first
   Serial.begin(115200);
+  nodeMCU.begin(115200);
+
+    //Ultrasonic for distance
+  pinMode (trigPin, OUTPUT);
+  pinMode (echoPin, INPUT);
+
+  //Mic setup
+  pinMode(micLPin, INPUT);
+  pinMode(micRPin, INPUT);
 
   //Accel for heading
   if (!accel.begin())
@@ -113,43 +127,36 @@ void setup() {
     while(1)
       ;
   }
-  accel.enableAutoRange(true);
+  accel.setRange(LSM303_RANGE_4G);
+  accel.setMode(LSM303_MODE_NORMAL);
 
-  //Ultrasonic for distance
-  pinMode (trigPin, OUTPUT);
-  pinMode (echoPin, INPUT);
 
-  //Mic setup
-  pinMode(micLPin, INPUT);
-  pinMode(micRPin, INPUT);
 
   //setting stepper speed
   stepper.setSpeed(20);
 
   //wifi setup
-  Serial.begin(115200);
-  nodeMCU.begin(115200);
-  Serial.print("ESP Uno MAC Board Address is:  ");
-  Serial.println(WiFi.macAddress());
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+  // Serial.print("ESP Uno MAC Board Address is:  ");
+  // Serial.println(WiFi.macAddress());
+  // WiFi.mode(WIFI_STA);
+  // WiFi.disconnect();
 
-  if (esp_now_init() != 0)
-  {
-    Serial.println("Error in Initializing ESP-NOW");
-  }
+  // if (esp_now_init() != 0)
+  // {
+  //   Serial.println("Error in Initializing ESP-NOW");
+  // }
 
-  // Set ESP-NOW Role
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+  // // Set ESP-NOW Role
+  // esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
-  // Register peer
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+  // // Once ESPNow is successfully Init, we will register for Send CB to
+  // // get the status of Trasnmitted packet
+  // esp_now_register_send_cb(OnDataSent);
+  // // Register peer
+  // esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
   
-  // Register for a callback function that will be called when data is received
-  esp_now_register_recv_cb(OnDataRecv);
+  // // Register for a callback function that will be called when data is received
+  // esp_now_register_recv_cb(OnDataRecv);
 }
 
 //------------------------------- Active Func -------------------------------
@@ -271,31 +278,66 @@ void micReading()
  */
 void wifiRun()
 {
+  bool check = false;
+
+  resetPacket();
+
+  //getting packet from master
   if(nodeMCU.available() != 0)
   {
+
     readIncomingData();
-    //Serial.println(wait);
+
+    if (newData)
+    {
+      readManual();
+      ready = true;
+      Serial.println(ready);
+    }
   }
+  
+  //on demand
+  if (ready)
+  {
+    String sender = "";
+    sender = convertPacketToString();
+    nodeMCU.println(sender);
+    check = true;
+  }
+
+  if(!check)
+  {
+    moveLocalToPacket();
+    String sender = "";
+    sender = convertPacketToString();
+    nodeMCU.println(sender);
+    Serial.println(sender);
+  }
+
+
+
+
+
 
   //sending to master?
-  if(ready)
-  {
-    Serial.println("Sending from master");
-    esp_now_send(broadcastAddress, (uint8_t *) &packet, sizeof(packet));
-    ready = false;
-  }
+  // if(ready)
+  // {
+  //   Serial.println("Sending from master");
+  //   esp_now_send(broadcastAddress, (uint8_t *) &packet, sizeof(packet));
+  //   ready = false;
+  // }
 
   //when getting stuff
-  if(received)
-  {
-    printIncomingReadings();
-    cli();
-    transferIncomingIntoPacket();
-    sei();
-    String sendMessage = convertPacketToString();
-    nodeMCU.println(sendMessage);
-    received = false;
-  }
+  // if(received)
+  // {
+  //   printIncomingReadings();
+  //   cli();
+  //   transferIncomingIntoPacket();
+  //   sei();
+  //   String sendMessage = convertPacketToString();
+  //   nodeMCU.println(sendMessage);
+  //   received = false;
+  // }
 }
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) 
@@ -330,19 +372,24 @@ void transferIncomingIntoPacket()
   packet.ack = incomingPacket.ack;
 }
 
+
 void readIncomingData()
 {
-  //Serial.println("We are reading something");
-  char temp = nodeMCU.read();
-  incomingData += String(temp);
-  if(temp == '>')
+  while(nodeMCU.available() > 0)
   {
-    Serial.println(incomingData);
-    String data = incomingData;
-    curData = incomingData;
-    incomingData  = "";
-    convertDataIntoPacket(data);
-    ready = true;
+    //Serial.println("We are reading something");
+    char temp = nodeMCU.read();
+    incomingData += String(temp);
+    if(temp == '>')
+    {
+      Serial.println(incomingData);
+      String data = incomingData;
+      curData = incomingData;
+      incomingData  = "";
+      convertDataIntoPacket(data);
+      newData = true;
+      break;
+    }
   }
 }
 
@@ -378,7 +425,7 @@ String convertPacketToString()
   converter += String(packet.leftMic);
   converter += ",";
   converter += String(packet.rightMic);
-  converter += ";";
+  converter += ",";
   converter += String(packet.heading);
   converter += ",";
   converter += String(packet.start);
@@ -418,7 +465,7 @@ void convertDataIntoPacket(String data)
       temp += ch;
     }
   }
-  //displayData();
+  displayData();
   populatePacketData();
 }
 
@@ -462,6 +509,57 @@ void displayData()
   Serial.println();
 }
 
+void moveLocalToPacket()
+{
+  packet.microphone_direction = micLocal;
+  packet.ultrasonic_distance = ultraLocal;
+  packet.leftMic = leftMicLocal;
+  packet.rightMic = rightMicLocal;
+  packet.heading = headingLocal;
+}
+
+void resetPacket()
+{
+  packet.microphone_direction = -1;
+  packet.ultrasonic_distance = 0;
+  packet.leftMic = false;
+  packet.rightMic = false;
+  packet.heading = 0;
+  packet.start = false;
+  packet.manual = 0;
+  packet.ack = false;
+}
+
+void readManual()
+{
+  int input = incomingPacket.manual;
+
+  //reset paket
+  resetPacket();
+
+  //0: auto , 1: motor, 2: distance, 3: L mic, 4: R mic, 5: compass
+  switch (input)
+  {
+    case 1:
+      //motor control
+      break;
+    case 2:
+      packet.ultrasonic_distance = ultraLocal;
+      break;
+    case 3:
+      packet.leftMic = leftMicLocal;
+      break;
+    case 4:
+      packet.rightMic = rightMicLocal;
+      break;
+    case 5:
+      packet.heading = headingLocal;
+      break;
+    default:
+      moveLocalToPacket();
+  }
+}
+
 
 void loop() {
   /*
@@ -469,11 +567,21 @@ void loop() {
   * Start to run all the sensor reading
   * Reset the stepper when done
   */
-  wifiRun();
-  headingReading();
+
+
+
+  Serial.println("Sensor");
+  // headingReading();
   ultraReading();
   micReading();
+
+  Serial.println("reset stepper");
   stepperReset();
+
+  Serial.println("wifi running");
+  wifiRun();
+
+  delay(7000);
 
   //we can cheat on the manual command
   //have the slave send all data into master
